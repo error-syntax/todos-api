9804e351764a50e17ab48a6524306f238c01bc14
Adam Age <adamwage@hotmail.com>
2023-11-03 13:28:35 -0500

    migrator to handle running generated migrations

---
+++ .drizzle/migrator.ts
@@ -0,0 +1,30 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import path from 'path';
+import pg from 'pg';
+
+import { envConfig } from '../src/config';
+
+(async function () {
+  console.log('Starting Migrations...')
+  try {
+    const dbConn = new pg.Pool({
+      database: envConfig.POSTGRES_DB || 'todoist',
+      host: '127.0.0.1',
+      port: +envConfig.POSTGRES_PORT! || 4500,
+      user: envConfig.POSTGRES_USER || 'apiuser',
+      password: envConfig.POSTGRES_PASSWORD || 'qwer9999',
+    });
+
+    const dbMigrator = drizzle(dbConn);
+
+    await migrate(dbMigrator, { migrationsFolder: path.resolve('.drizzle', 'migrations') })
+
+    console.log('Migration Successful!');
+
+    process.exit(0);
+  } catch (error) {
+    console.log('Migration Error', error)
+    process.exit(0);
+  }
+})();
